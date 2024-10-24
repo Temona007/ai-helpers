513c50ff036503ab3d175af09e5a8fd507458fa6
Art R. <temona007@gmail.com>
2024-10-24 15:31:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: './.env' });
 
 const express = require('express');
 const cors    = require('cors');
+const path    = require('path');
 
 const app = express();
 
@@ -10,19 +11,20 @@ app.use(express.json());
 
 // Make folder avalieable for all
 app.use(express.static('public'));
-
+// App routes
 app.use('/api/openai', require('./routes/openai')); 
+
+// Serve static files from the React app's build directory
+app.use(express.static(path.join(__dirname, 'client/build')));
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+// Handle other routes and return the React app's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 const port = process.env.PORT || 4242;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
-// ADDED
-
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// Handles any requests that don't match the ones above
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
